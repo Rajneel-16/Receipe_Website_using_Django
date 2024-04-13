e927e6a4ebb5faad351fa33d5e60c75bd5b5963e
Rajneel-16 <rajneelwagh@gmail.com>
2024-04-13 23:37:58 +0530

    hogaya saab

--- myvege/urls.py
+++ myvege/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     
     path('receipes/',receipes,name='receipes'),
 
-    # path('',receipes,name='receipes'),
+    path('',receipes,name='receipes'),
     path('delete-receipe/<id>/',delete_receipe,name='delete_receipe'),
     path('update-receipe/<id>/',update_receipe,name='update_receipe'),
 
